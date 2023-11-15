00f3812913a1bf7eb5329c3fd580b48f83f33f72
Kennedy Mindeman <kennedymindeman@gmail.com>
2023-11-15 13:54:54 -0600

    Change test names to be detected by pytest

--- test.py
+++ test.py
@@ -18,18 +18,18 @@ def test_play_x_on_empty_board():
     board.play_o((1, 1))
 
 
-def play_round():
+def test_play_round():
     board = TicTacToeBoard()
     board.play_x((0, 0))
     board.play_o((1, 1))
 
 
-def play_move_on_occupied_tile():
+def test_play_move_on_occupied_tile():
     board = TicTacToeBoard()
     board.play_x((0, 0))
     pytest.raises(ValueError, board.play_o, (0, 0))
 
 
-def play_move_off_board_tile():
+def test_play_move_off_board_tile():
     board = TicTacToeBoard()
-    pytest.raises(ValueError, board.play_x, (0, 1))
+    pytest.raises(ValueError, board.play_x, (0, -1))
